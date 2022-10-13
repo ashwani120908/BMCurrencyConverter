f765b8118338d49813b7e9cf54645c6511992f1d
ashwani120908 <ashwani120908@gmail.com>
2022-10-13 17:03:33 +0400

    removed unued imports

--- app/src/main/java/com/demo/bmcurrencyconverter/network/ApiService.kt
+++ app/src/main/java/com/demo/bmcurrencyconverter/network/ApiService.kt
@@ -1,7 +1,6 @@
 package com.demo.bmcurrencyconverter.network
 
 import com.demo.bmcurrencyconverter.models.LatestRates
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
