ca7219aca9d8a2d660e37a82b93ef9ae1a8be008
Shivangi <shivangi@28lightbulbs.com>
2022-10-19 15:13:11 +0400

    Update ApiHelperImpl.kt

--- app/src/main/java/com/demo/bmcurrencyconverter/network/ApiHelperImpl.kt
+++ app/src/main/java/com/demo/bmcurrencyconverter/network/ApiHelperImpl.kt
@@ -9,7 +9,7 @@ class ApiHelperImpl @Inject constructor(private val apiService: ApiService) : Ap
     companion object {
         const val BASE_URL = "https://api.apilayer.com/fixer/"
 
-        const val ACCESS_KEY = "UBPmX3iNFtaHzviNyAj72RSkSbSV6gKq"
+        const val ACCESS_KEY = "6zL4EF6R1gEu01ZLb4CZ5cIFeoErzqC1"
     }
 
     override suspend fun getLatestRates(): LatestRates =
